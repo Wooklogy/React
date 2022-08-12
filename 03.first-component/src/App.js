631cbd116466602b87d6947cd14a4bbae619e88a
wkdgusdnr55@gmail.com <wkdgusdnr55@gmail.com>
2022-08-12 13:15:05 +0900

    03.리액트 살펴보기

---
+++ 03.first-component/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Hello from "./Hello";
+function App() {
+    return (
+        <div className="App">
+            <Hello></Hello>
+            <Hello></Hello>
+            <Hello></Hello>
+            <Hello></Hello>
+            <Hello></Hello>
+            {/* 또는 <Hello />*/}
+        </div>
+    );
+}
+
+export default App;
