c53f2a609144993e5e2071c42b060f567d2b1d99
wkdgusdnr55@gmail.com <wkdgusdnr55@gmail.com>
2022-08-15 10:28:17 +0900

    05.Properties

---
+++ 05.properties/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Hello from "./components/Hello";
+import Hi from "./components/Hi";
+import Wrapper from "./components/Wrapper";
+
+function App() {
+    return (
+        <Wrapper>
+            <Hello name="react" />
+            <Hi text="안녕 파랑색" color="blue" />
+            <Hi></Hi>
+        </Wrapper>
+    );
+}
+
+export default App;
